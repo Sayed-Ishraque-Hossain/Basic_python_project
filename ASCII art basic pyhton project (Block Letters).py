06c4ecf3e4a99b682adef7b83a0bcc7d07d28217
Sayed-Ishraque-Hossain <78308824+Sayed-Ishraque-Hossain@users.noreply.github.com>
2021-02-22 17:39:55 +0600

    Add files via upload

---
+++ ASCII art basic pyhton project (Block Letters).py
@@ -0,0 +1,15 @@
+print("ASCII art basic python project: Block Letters.")
+
+
+
+
+print('  SSSS     H       H')
+print('S      S   H       H')
+print('S          H       H')
+print('S          H       H')
+print('  SSSSS    H H H H H')
+print('       S   H       H')
+print('       S   H       H')
+print('       S   H       H')
+print('S      S   H       H')
+print(' SSSSSS    H       H')
